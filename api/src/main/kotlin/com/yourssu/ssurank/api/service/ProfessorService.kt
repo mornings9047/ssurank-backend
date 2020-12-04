85dc27e2fae0c62131153121e6160f6ff0c0c2e7
JoonHee <mornings9047@naver.com>
2020-12-04 14:39:33 +0900

    ProfessorRepository.save()의 리턴 타입을 Professor로 수정

--- api/src/main/kotlin/com/yourssu/ssurank/api/service/ProfessorService.kt
+++ api/src/main/kotlin/com/yourssu/ssurank/api/service/ProfessorService.kt
@@ -1,12 +1,10 @@
 package com.yourssu.ssurank.api.service
 
 import com.yourssu.ssurank.api.repository.model.dataAccess.ssurank.ProfessorDataAccessor
-import com.yourssu.ssurank.api.repository.model.dataAccess.ssurank.ProfessorRepository
 import com.yourssu.ssurank.api.repository.model.entity.ssurank.SimplifiedProfessor
 import com.yourssu.ssurank.api.repository.model.function.ssurank.GetHonorProfessorsFunction
 import com.yourssu.ssurank.api.repository.model.function.ssurank.SearchProfessorFunction
 import com.yourssu.ssurank.api.repository.model.projection.ssurank.ProfessorTransporter
-import com.yourssu.ssurank.api.response.GetHonorProfessorResponse
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
