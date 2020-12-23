c969df05cd5f091f7ae69028dfb430f3528f0d50
Reno <reno.urssu@gmail.com>
2020-12-23 16:12:27 +0900

    init branch

---
+++ api/src/main/kotlin/com/yourssu/ssurank/api/repository/model/entity/ssurank/CourseEvaluation.kt
@@ -0,0 +1,39 @@
+package com.yourssu.ssurank.api.repository.model.entity.ssurank
+
+import com.yourssu.ssurank.api.repository.model.entity.common.SuperEntity
+import java.util.*
+import javax.persistence.*
+
+@Table(name = "Course_Evaluation")
+data class CourseEvaluation(
+        @field:Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        override val id: Int? = null,
+
+        @Column(nullable = false)
+        val email: String,
+
+        @Column(nullable = false)
+        val type: Int,
+
+        @Column(nullable = false)
+        val year: Int,
+
+        @Column(nullable = false)
+        val semester: Semester,
+
+        @Column(nullable = false)
+        val content: String,
+
+        @Column(nullable = false)
+        val thumbs_up : Int,
+
+        @Column(nullable = false)
+        val thumbs_down : Int,
+
+        @Column(nullable = false)
+        val createAt: Date,
+
+        @Column(nullable = false)
+        val isDeleted: Boolean? = null
+) : SuperEntity<Int>
