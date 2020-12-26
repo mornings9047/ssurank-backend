5b2a14b5ea34e69f2aba5f6d428e46e5707f598a
Reno <reno.urssu@gmail.com>
2020-12-26 14:01:57 +0900

    학과 줄임말 목록 가져오기 수정

--- api/src/main/kotlin/com/yourssu/ssurank/api/repository/model/function/ssurank/GetDepartmentListFunction.kt
+++ api/src/main/kotlin/com/yourssu/ssurank/api/repository/model/function/ssurank/GetDepartmentListFunction.kt
@@ -6,7 +6,7 @@ import com.yourssu.ssurank.api.repository.model.projection.ssurank.DepartmentTra
 class GetDepartmentListFunction(
         private val departmentDataAccessor: DepartmentDataAccessor
 ) {
-    private val colleges = arrayOf("IT대학", "공과대학", "인문대", "자연대", "공과대학", "사회과학대학", "경제통상대학", "경영대학", "베어드교양대학", "융특")
+    private val colleges = arrayOf("IT대학", "공과대학", "인문대학", "자연과학대학", "공과대학", "사회과학대학", "경제통상대학", "경영대학", "베어드교양대학", "융특")
 
     fun getDepartmentList(): Map<String, List<DepartmentTransporter>> {
         val deptList: MutableMap<String, List<DepartmentTransporter>> = hashMapOf()
